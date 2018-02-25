03b44e295696118b0472c23c39f76f35e3646e02
Deric <deric.buettner@gmail.com>
2018-02-25 18:38:33 +0100

    Add adsr envelope

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
+import AdsrGain from 'adsr-gain-node';
+
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
 const ctx = new AudioContext();
 
 const osc = ctx.createOscillator();
-const gainNode = ctx.createGain();
-
-osc.connect(gainNode);
-gainNode.connect(ctx.destination);
-
 
 osc.type = 'sine';
 osc.frequency.setValueAtTime(440, ctx.currentTime);
+
+const adsrNodeCreator = new AdsrGain(ctx, {
+  initGain: 0.1,
+  maxGain: 0.4,
+  attackTime: 0.1,
+  sustainTime: 0.2,
+  releaseTime: 0.5,
+});
+const triggerTime = 0;
+
+const env = adsrNodeCreator.getGainNode(triggerTime);
+
+osc.connect(env);
+env.connect(ctx.destination);
+
 osc.start();
-
-
-var maxVol = 0.02;
-gainNode.gain.value = initialVol;
