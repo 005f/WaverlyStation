92ad0b47a81f5cb1aa8156edabc791bfed80e24f
Deric <deric.buettner@gmail.com>
2018-02-25 18:54:46 +0100

    Add filter

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
 const ctx = new AudioContext();
 
+
+// Oscillator
 const osc = ctx.createOscillator();
 
 osc.type = 'sine';
 osc.frequency.setValueAtTime(440, ctx.currentTime);
 
+
+// Filter
+const filter = ctx.createBiquadFilter();
+
+filter.type = filter.LOWPASS;
+filter.frequency.value = 2500;
+filter.Q.value = 10;
+
+
+// Envelope
 const adsrNodeCreator = new AdsrGain(ctx, {
   initGain: 0.1,
   maxGain: 0.4,
@@ -17,10 +29,12 @@ const adsrNodeCreator = new AdsrGain(ctx, {
   releaseTime: 0.5,
 });
 const triggerTime = 0;
-
 const env = adsrNodeCreator.getGainNode(triggerTime);
 
-osc.connect(env);
+
+// Connect
+osc.connect(filter);
+filter.connect(env);
 env.connect(ctx.destination);
 
 osc.start();
