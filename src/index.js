ea74acebfced1b7b170ebffa0485baf56332a70e
Deric <deric.buettner@gmail.com>
2018-02-27 20:38:37 +0100

    Replace envelope with better lib

--- src/index.js
+++ src/index.js
@@ -1,40 +1,74 @@
-import AdsrGain from 'adsr-gain-node';
+import ADSREnvelope from "adsr-envelope";
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
 const ctx = new AudioContext();
 
+let env = new ADSREnvelope();
 
-// Oscillator
-const osc = ctx.createOscillator();
+let filter;
+let gain;
+let osc;
+let startTime;
 
-osc.type = 'sine';
-osc.frequency.setValueAtTime(440, ctx.currentTime);
+let keylocked = false;
 
+function startPlay() {
 
-// Filter
-const filter = ctx.createBiquadFilter();
+  startTime = 0;
 
-filter.type = filter.LOWPASS;
-filter.frequency.value = 2500;
-filter.Q.value = 10;
+  // Oscillator
+  osc = ctx.createOscillator();
 
+  osc.type = 'sine';
+  osc.frequency.setValueAtTime(440, ctx.currentTime);
 
-// Envelope
-const adsrNodeCreator = new AdsrGain(ctx, {
-  initGain: 0.1,
-  maxGain: 0.4,
-  attackTime: 0.1,
-  sustainTime: 0.2,
-  releaseTime: 0.5,
-});
-const triggerTime = 0;
-const env = adsrNodeCreator.getGainNode(triggerTime);
+  osc.onended = () => {
+    osc.disconnect();
+    gain.disconnect();
+  }
 
+  // Filter
+  filter = ctx.createBiquadFilter();
 
-// Connect
-osc.connect(filter);
-filter.connect(env);
-env.connect(ctx.destination);
+  filter.type = 'lowpass';
+  filter.frequency.value = 500;
+  filter.Q.value = 20;
 
-osc.start();
+  gain = ctx.createGain();
+
+  // Connect
+  osc.connect(filter);
+  filter.connect(gain);
+  gain.connect(ctx.destination);
+
+  document.addEventListener('keydown', handleKeydown);
+  document.addEventListener('keyup', handleKeyup);
+}
+
+const handleKeydown = () => {
+  if (!keylocked) {
+    startTime = ctx.currentTime;
+
+    env.gateTime = Infinity;
+    env.applyTo(gain.gain, startTime);
+    osc.start(startTime);
+
+    keylocked = true;
+  }
+};
+
+const handleKeyup = () => {
+  keylocked = false;
+  gain.gain.cancelScheduledValues(startTime);
+  env.gateTime = ctx.currentTime - startTime;
+  env.applyTo(gain.gain, startTime);
+  osc.stop(startTime + env.duration);
+
+  document.removeEventListener('keydown', handleKeydown);
+  document.removeEventListener('keyup', handleKeyup);
+
+  startPlay();
+}
+
+startPlay();
