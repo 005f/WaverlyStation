e5dea1b3c27fa67ce672bc9731a7774c0737772a
Deric <deric.buettner@gmail.com>
2018-04-14 20:01:28 +0200

    Add linting to project

--- src/synthEngine.js
+++ src/synthEngine.js
@@ -1,73 +1,71 @@
-import ADSREnvelope from "adsr-envelope";
+import ADSREnvelope from 'adsr-envelope'
 
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
+window.AudioContext = window.AudioContext || window.webkitAudioContext
 
-const ctx = new AudioContext();
+const ctx = new AudioContext()
 
-const lockedKeys = {};
+const lockedKeys = {}
 
 
 function playNote(note) {
-  // Oscillator
-  const osc = ctx.createOscillator();
-  osc.type = 'sine';
+  const osc = ctx.createOscillator()
+  osc.type = 'sine'
   // Temporary, crude simulation of actual notes
-  osc.frequency.setValueAtTime(note * 10, ctx.currentTime);
+  osc.frequency.setValueAtTime(note * 10, ctx.currentTime)
 
-  const filter = ctx.createBiquadFilter();
-  filter.type = 'lowpass';
-  filter.frequency.value = 500;
-  filter.Q.value = 20;
+  const filter = ctx.createBiquadFilter()
+  filter.type = 'lowpass'
+  filter.frequency.value = 500
+  filter.Q.value = 20
 
   const env = new ADSREnvelope({
-    decayTime:0.6,
+    decayTime: 0.6,
     decayCurve: 'exp',
     sustainLevel: 0.05,
     releaseTime: 0.3,
     peakLevel: 0.08,
-  });
-  env.gateTime = Infinity;
-  const gain = ctx.createGain();
-  env.applyTo(gain.gain, ctx.currentTime);
+  })
+  env.gateTime = Infinity
+  const gain = ctx.createGain()
+  env.applyTo(gain.gain, ctx.currentTime)
 
-  // Connect
-  osc.connect(filter);
-  filter.connect(gain);
-  gain.connect(ctx.destination);
+  osc.connect(filter)
+  filter.connect(gain)
+  gain.connect(ctx.destination)
 
-  osc.start(ctx.currentTime);
+  osc.start(ctx.currentTime)
 
-  const startTime = ctx.currentTime;
+  const startTime = ctx.currentTime
 
   const releaseNote = () => {
-    gain.gain.cancelScheduledValues(startTime);
-    env.gateTime = ctx.currentTime - startTime;
-    env.applyTo(gain.gain, startTime);
+    gain.gain.cancelScheduledValues(startTime)
+    env.gateTime = ctx.currentTime - startTime
+    env.applyTo(gain.gain, startTime)
 
-    osc.stop(startTime + env.duration);
+    osc.stop(startTime + env.duration)
 
-    lockedKeys[note] = false;
+    lockedKeys[note] = false
   }
 
-  document.addEventListener('keyup', releaseNote);
+  document.addEventListener('keyup', releaseNote)
 
   // Clean up after note is finished playing
   osc.onended = () => {
-    document.removeEventListener('keyup', releaseNote);
-    osc.disconnect();
-    gain.disconnect();
+    document.removeEventListener('keyup', releaseNote)
+    osc.disconnect()
+    gain.disconnect()
   }
 }
 
 const handleKeydown = (e) => {
   // keydown event fires multiple times so we only handle the first event
   if (!lockedKeys[e.keyCode]) {
-    lockedKeys[e.keyCode] = true;
+    lockedKeys[e.keyCode] = true
 
-    playNote(e.keyCode);
+    playNote(e.keyCode)
   }
-};
+}
 
 export default function initSynth() {
-  document.addEventListener('keydown', handleKeydown);
+  document.addEventListener('keydown', handleKeydown)
 }
